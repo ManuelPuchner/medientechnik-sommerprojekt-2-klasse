a2f3d55cd7fb8aa44519d10e566e5ff05db73dc9
ManuelPuchner <75803474+ManuelPuchner@users.noreply.github.com>
2022-04-25 15:05:32 +0200

    commented out the color pickers

--- pages/configurator/ConfigurationElements.jsx
+++ pages/configurator/ConfigurationElements.jsx
@@ -9,7 +9,7 @@ const InputWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 10px;
-`
+`;
 
 export default function ConfigurationElements({ colors, setColors }) {
   return (
@@ -19,53 +19,59 @@ export default function ConfigurationElements({ colors, setColors }) {
         <div>
           <InputWrapper>
             <label>Laces</label>
-            <ColorPicker
+            <input
+              type="color"
+              name=""
+              id=""
               onChange={(e) => setColors({ ...colors, laces: e.target.value })}
             />
+            {/* <ColorPicker
+              onChange={(e) => setColors({ ...colors, laces: e.target.value })}
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Mesh</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) => setColors({ ...colors, mesh: e.target.value })}
-            />
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Caps</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) => setColors({ ...colors, caps: e.target.value })}
-            />
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Inner</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) => setColors({ ...colors, inner: e.target.value })}
-            />
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Sole</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) => setColors({ ...colors, sole: e.target.value })}
-            />
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Stripes</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) =>
                 setColors({ ...colors, stripes: e.target.value })
               }
-            />
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Band</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) => setColors({ ...colors, band: e.target.value })}
-            />
+            /> */}
           </InputWrapper>
           <InputWrapper>
             <label>Patch</label>
-            <ColorPicker
+            {/* <ColorPicker
               onChange={(e) => setColors({ ...colors, patch: e.target.value })}
-            />
+            /> */}
           </InputWrapper>
         </div>
       </ConfigurationWrapper>
